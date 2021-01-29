ec01964123382ba596f30879c7816be7f238fe16
asyy <alexys.guerin@hotmail.fr>
2021-01-29 13:39:06 +0100

    Correction ListModel

--- src/main/java/data/TableViewModel.java
+++ src/main/java/data/TableViewModel.java
@@ -4,6 +4,8 @@ package data;
 
 import org.zkoss.json.JSONArray;
 import org.zkoss.json.parser.JSONParser;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.ListModelList;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -24,6 +26,10 @@ public class TableViewModel {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public ListModel<TableLine> getListModel(){
+        return new ListModelList<TableLine>(table.getTable());
     }
 
     public static void main(String[] args) {
